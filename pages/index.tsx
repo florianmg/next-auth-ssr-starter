99206476c99fd86d4e65df1d60601f789fe2a413
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2022-01-07 20:14:18 +0100

    fix: remove useless modal component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,8 @@
 import type { NextPage } from 'next';
-import Modal from '../components/modal';
 
 const Home: NextPage = () => {
   return (
     <main>
-      <Modal>
-        <>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
-            eum hic vel adipisci a explicabo veniam similique ipsum id molestiae
-            ex minus ut cum fugit suscipit cupiditate tempore, saepe facere.
-          </p>
-          <br />
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
-            eum hic vel adipisci a explicabo veniam similique ipsum id molestiae
-            ex minus ut cum fugit suscipit cupiditate tempore, saepe facere.
-          </p>
-          <br />
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
-            eum hic vel adipisci a explicabo veniam similique ipsum id molestiae
-            ex minus ut cum fugit suscipit cupiditate tempore, saepe facere.
-          </p>
-        </>
-      </Modal>
       <h1>Home page</h1>
     </main>
   );
