6e5bd1a329f4e255e0fa30a50b5837061f5fb1be
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2022-01-09 17:20:32 +0100

    feat: Toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import '../styles/variables.scss';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../store/authContext';
 import Navbar from '../components/navbar';
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </AuthProvider>
   );
 }
