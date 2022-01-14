b76207f4ca1119e390c074d25935e7b035c73bd3
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2022-01-14 14:00:49 +0100

    fix: remove useless code

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -31,18 +31,6 @@ const Modal: React.FC<IModalProps> = ({
       ></button>
     </div>
   );
-  return (
-    <div className={styles.container}>
-      <div className={styles.content} style={{ maxWidth }}>
-        <span className={styles['close-btn-container']}>
-          <span role="button" className={styles.icon} onClick={onClose}>
-            <CgCloseO />
-          </span>
-        </span>
-        {children}
-      </div>
-    </div>
-  );
 };
 
 export default Modal;
