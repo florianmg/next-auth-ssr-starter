ad7c95e5537dcb3d24a112ea6b89ada9a20d4323
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2022-01-14 19:43:40 +0100

    feat: navbar container

--- components/navbar/LoggedNavigation.tsx
+++ components/navbar/LoggedNavigation.tsx
@@ -8,34 +8,36 @@ const LoggedNavigation: React.FC = () => {
 
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <Link href="/">
-          <a className="navbar-item">LOGO</a>
-        </Link>
-        <a
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
+      <div className="container">
+        <div className="navbar-brand">
+          <Link href="/">
+            <a className="navbar-item">LOGO</a>
+          </Link>
+          <a
+            role="button"
+            className="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start"></div>
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <Link href="/dashboard">
-                <a className="navbar-item">{t('common:dashboard')}</a>
-              </Link>
-              <a className="navbar-item" onClick={logout}>
-                {t('auth:logout')}
-              </a>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start"></div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <Link href="/dashboard">
+                  <a className="navbar-item">{t('common:dashboard')}</a>
+                </Link>
+                <a className="navbar-item" onClick={logout}>
+                  {t('auth:logout')}
+                </a>
+              </div>
             </div>
           </div>
         </div>
