13751a18c313e277f53409bf41acca9c820ca41a
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2022-01-14 08:12:37 +0100

    feat: install bulma and delete reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.scss';
-import '../styles/reset.css';
 import '../styles/variables.scss';
+
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { AuthProvider } from '../store/authContext';
