ade93fb1d89a816efb1918edfc662621df200b0f
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2022-01-16 16:42:17 +0100

    feat: className + onCLick

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,13 +1,22 @@
 interface IButtonProps {
   type: 'submit' | 'button';
   value: string;
+  onClick?: () => void;
   disabled?: boolean;
+  className?: string;
 }
 
-const Button: React.FC<IButtonProps> = ({ type, value, disabled = false }) => {
+const Button: React.FC<IButtonProps> = ({
+  type,
+  value,
+  disabled = false,
+  className = '',
+  onClick,
+}) => {
   return (
-    <div className="field">
+    <div className={`field ${className}`}>
       <button
+        onClick={onClick}
         className="button is-primary is-fullwidth"
         type={type}
         disabled={disabled}
